477bdd9e57f6af0e459f991322b73640fae37143
celia babu <85995015+CeliaBabu2002@users.noreply.github.com>
2023-06-13 15:59:32 +0530

    login-update

--- Login-Register-main/login.php
+++ Login-Register-main/login.php
@@ -39,42 +39,16 @@ if (isset($_SESSION["user"])) {
         }
         ?>
       <form action="login.php" method="post">
-        <div class="box">
-            <div class="row">
-                <div class="col-sm-5 col-xs-1 box1">
-                    <div class="inline-text">
-                        <h1><b><i>Login</i></b></h1>
-                        <p><b>Get Access to your orders,<br>
-                            Wishlist and<br>
-                            Recommendations<br>
-                        </b></p>
-                        <pre>
-
-                        </pre>
-                        <div style="text-align: center;">
-                            <img src="logob.png" width="250" alt="My Image" />
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-5 col-xs-1 box2">
-                    <div class="form-group">
-                        <input type="email" placeholder="Enter Email:" name="email" class="form-control">
-                    </div>
-                    <div class="form-group">
-                        <input type="password" placeholder="Enter Password:" name="password" class="form-control">
-                    </div>
-                    <div class="user-id">
-                            <p>By continuing, you agree to Agri-Bussiness's terms of use and Privacy Policy </p>
-                    </div>
-                    <div class="form-btn">
-                        <input type="submit" value="Login" name="login" class="btn btn-primary">
-                    </div>
-                    <div class="user-id">
-                            <p class="footer"><a href="#">New to Agri-Bussiness? Create an account</a></p>
-                    </div>
-                </div>
-            </div>
-        </form>
+        <div class="form-group">
+            <input type="email" placeholder="Enter Email:" name="email" class="form-control">
+        </div>
+        <div class="form-group">
+            <input type="password" placeholder="Enter Password:" name="password" class="form-control">
+        </div>
+        <div class="form-btn">
+            <input type="submit" value="Login" name="login" class="btn btn-primary">
+        </div>
+      </form>
      <div><p>Not registered yet <a href="registration.php">Register Here</a></p></div>
     </div>
 </body>
